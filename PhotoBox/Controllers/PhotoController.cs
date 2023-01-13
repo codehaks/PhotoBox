d6677d03dd5c0907480952a06aa9760fd236b423
Hakim Ghods <ghods.hakim@hotmail.com>
2023-01-14 02:33:15 +0330

    Add project files.

---
+++ PhotoBox/Controllers/PhotoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhotoBox.Controllers;
+
+public class PhotoController : ControllerBase
+{
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    public PhotoController(IWebHostEnvironment webHostEnvironment)
+    {
+        _webHostEnvironment = webHostEnvironment;
+    }
+
+    [Route("api/photo/{**filename}")]
+    public IActionResult Get(string fileName)
+    {
+        var path = Path.Combine(_webHostEnvironment.ContentRootPath, "Pics",fileName);
+
+        var content=System.IO.File.ReadAllBytes(path);
+
+        return File(content,"image/jpeg");
+    }
+}
---
+++ PhotoBox/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PhotoBox.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment webHostEnvironment)
+    {
+        _logger = logger;
+        _webHostEnvironment = webHostEnvironment;
+    }
+
+    public IList<string> FileList { get; set; }
+    public void OnGet()
+    {
+        var path = Path.Combine(_webHostEnvironment.ContentRootPath, "Pics");
+
+        FileList = Directory.GetFiles(path).OrderBy(f=> f).Select(f=> new FileInfo(f).Name).ToList();
+    }
+}
